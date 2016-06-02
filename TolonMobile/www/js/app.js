ca0cafa9d17a73b63e1d2102d33ec5bb34d7f1d8
cllCodeGeek <celal@bilims.om>
2016-06-02 14:21:32 +0300

    Modified Css and Js

--- TolonMobile/www/js/app.js
+++ TolonMobile/www/js/app.js
@@ -141,7 +141,7 @@ angular.module('starter', ['ionic'])
       // Don't remove this line unless you know what you are doing. It stops the viewport
       // from snapping when text inputs are focused. Ionic handles this internally for
       // a much nicer keyboard experience.
-      cordova.plugins.Keyboard.disableScroll(true);
+      cordova.plugins.Keyboard.disableScroll(false);
     }
     if(window.StatusBar) {
       StatusBar.styleDefault();
